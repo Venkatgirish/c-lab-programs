dcc3e401230888286154796e3ff4a417b44b5437
venkatagirish <112752244+Venkatgirish@users.noreply.github.com>
2022-10-04 10:29:10 +0530

    Add files via upload

---
+++ eligibility.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter your age: "<<endl;
+	cin>>age;
+	if(age>=7)
+	{
+		cout<<"you are allowed to  vote"<<endl;
+	}
+	else
+	{
+		cout<<"invalid && you are not allowed to vote"<<endl;
+}
+}
